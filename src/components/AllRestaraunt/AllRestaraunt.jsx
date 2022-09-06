11285c25dc688f038184fc1e82d6e8b7b4149a57
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-09-06 17:07:58 +0300

    style background

--- src/components/AllRestaraunt/AllRestaraunt.jsx
+++ src/components/AllRestaraunt/AllRestaraunt.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import '../AllRestaraunt/AllRestaraunt.scss'
+ import bg from '../../assets/restaraunt.jpg'
 
 function AllRestaraunts() {
+
+  const myStyle={
+    
+      width: '100%',
+      height: '100vh',
+      backgroundImage:'linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)),url(../../assets/{restaraunt.jpg})',
+      backgroundSize: 'cover',
+      backgroundPosition:'center',
+  
+ 
+};
   return (
-    <div >
+
+    <div style={myStyle}>
       all restaraunts
     </div>
   )
