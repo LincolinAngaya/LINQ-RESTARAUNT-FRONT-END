877ccc2cd48ec314044eb1a7320eb85a4b52fd29
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-09-06 11:13:50 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 
+import {Route,Switch} from 'react-router-dom';
+
+import AllRestaraunts from './components/landing/AllRestaraunts';
+import NewRestaraunts from './components/landing/NewRestaraunts';
+import Login from './components/landing/Login';
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello front end</h1>
+      <Switch>
+      <Route exact path='/'>
+        <AllRestaraunts />
+      </Route>
+      <Route path='/new-restaraunt'>
+        <NewRestaraunts />
+      </Route>
+      <Route path='/login'>
+        <Login />
+      </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/landing/AllRestaraunts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AllRestaraunts() {
+  return (
+    <div>
+      all restaraunts
+    </div>
+  )
+}
+
+export default AllRestaraunts
