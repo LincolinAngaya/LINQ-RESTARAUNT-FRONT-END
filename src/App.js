dd6a12ee7310a9d324a51bac83e8fc09b9aab543
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-09-06 15:29:43 +0300

    new files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 
 import {Route,Switch} from 'react-router-dom';
+import './App.scss'
 
 import AllRestaraunts from './components/landing/AllRestaraunts';
 import NewRestaraunts from './components/landing/NewRestaraunts';
 import Login from './components/landing/Login';
-// import Navbar from './components/landing/Navbar';
+ import Navbar from './components/landing/Navbar/Navbar';
 
 function App() {
   return (
     <div>
-      {/* <Navbar /> */}
+     <Navbar /> 
       <Switch>
       <Route exact path='/'>
         <AllRestaraunts />
