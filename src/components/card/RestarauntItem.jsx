fba6a17f0618d43e7ab3381b0576f6efef09e0e0
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-09-06 18:10:34 +0300

    add more info

--- src/components/card/RestarauntItem.jsx
+++ src/components/card/RestarauntItem.jsx
@@ -7,12 +7,25 @@ function RestarauntItem() {
      <img 
       width="200"
       src ="https://images.pexels.com/photos/70441/pexels-photo-70441.jpeg?auto=compress&cs=tinysrgb&w=400"  alt="Miami" />
+      <span className='close-btn'>
+         <i class="material-symbols-outlined">cancel_presentation</i>
+      </span>
     </div>
-    <span className='close-btn'>
-    <span class="material-symbols-outlined">
-cancel_presentation
-</span>
-    </span>
+
+    <div className='restaraunt-info'>
+      <h3>City</h3>
+      <h4>Name</h4>
+      <h5>phone no</h5>
+      <h6>
+        info{""}
+        <span>
+             <i className="material-symbols-outlined">expand_more</i>
+        </span>
+        </h6>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores minima eos corrupti velit, deserunt repellendus. 
+        </p>
+    </div>
+    
 
     </article>
     
