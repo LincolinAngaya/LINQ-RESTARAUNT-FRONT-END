479ab18b30393f9abf9e1765565fb1607d8bb2ee
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-09-07 12:07:54 +0300

    modify the fields

--- src/components/NewRestaraunt/NewRestaraunt.jsx
+++ src/components/NewRestaraunt/NewRestaraunt.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
-import '../NewRestaraunt.scss'
+ import '../../NewRestarauntform.scss'
 
 function NewRestaraunts() {
   return (
-    <div className='Card'>
+    <div className='card'>
     <form className='form' >
     <div className='control'>
-            <label htmlFor='image'>Movie Image</label>
+            <label htmlFor='image'>Restaraunt Image</label>
             <input type="url"  required id="image" />
-        </div>
+     </div>
     <div className='control'>
-            <label htmlFor='title'>Movie Title</label>
-            <input type="text"  required id="title" r/>
+            <label htmlFor='text'>Restaraunt Name</label>
+            <input type="text"  required id="name" />
         </div>
         <div className='control'>
-            <label htmlFor='type'>Movie Type</label>
-            <input type="text"  required id="type" />
+            <label htmlFor='location'>Restaraunt Location</label>
+            <input type="text"  required id="location" />
         </div>
         <div className='control'>
-            <label htmlFor='year'>Movie Year</label>
-            <input type="text"  required id="year" />
+            <label htmlFor='number'>Phone Number</label>
+            <input type="text"  required id="number" />
         </div>
         <div className='control'>
-            <label htmlFor='description'>Movie Trailer</label>
+            <label htmlFor='description'>Restaraunt Description</label>
             <textarea id="description" required rows='5' />
         </div>
         <div className='actions'>
-            <button>Add New Movie</button>
+            <button>Add New Restaraunt</button>
         </div>
     </form>
   </div>
