bac367f620b28741db41cabe37dbd8ff06463300
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-09-07 14:24:27 +0300

    add span for user and timstamp

--- src/components/Reviews/Reviewmsg.jsx
+++ src/components/Reviews/Reviewmsg.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import EditReview from './EditReview'
+import '../Reviews/Review.scss'
 
 function Review() {
   return (
+    <li>
+      <span className="user">{username}</span>
+      <span className="time">{timestamp}</span>
     <EditReview />
+    </li>
   )
 }
 
