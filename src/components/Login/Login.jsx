586f7aef9018bff3a89c7643fef96eb8433fe598
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-09-07 17:01:16 +0300

    add logi form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,28 +6,17 @@ function Login() {
   return (
     <div className='card'>
     <form className='form' >
+    
     <div className='control'>
-            <label htmlFor='image'>Restaraunt Image</label>
-            <input type="url"  required id="image" />
-     </div>
-    <div className='control'>
-            <label htmlFor='text'>Restaraunt Name</label>
-            <input type="text"  required id="name" />
+            <label htmlFor='text'>User Name</label>
+            <input type="text"  required id="username" />
         </div>
         <div className='control'>
-            <label htmlFor='location'>Restaraunt Location</label>
-            <input type="text"  required id="location" />
-        </div>
-        <div className='control'>
-            <label htmlFor='number'>Phone Number</label>
-            <input type="text"  required id="number" />
-        </div>
-        <div className='control'>
-            <label htmlFor='description'>Restaraunt Description</label>
-            <textarea id="description" required rows='5' />
+            <label htmlFor='password'>Password</label>
+            <input type="password"  required id="password" />
         </div>
         <div className='actions'>
-            <button>Add New Restaraunt</button>
+            <button>Log in</button>
         </div>
     </form>
   </div>
