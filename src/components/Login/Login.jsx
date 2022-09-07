0ebcc9b5df71f2883342fcc3f3e00dfcfe7fbc0c
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-09-07 17:07:39 +0300

    change the styling

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,18 +4,18 @@ import '../../NewRestarauntform.scss'
 
 function Login() {
   return (
-    <div className='card'>
-    <form className='form' >
+    <div className='card1'>
+    <form className='form1' >
     
-    <div className='control'>
+    <div className='control1'>
             <label htmlFor='text'>User Name</label>
             <input type="text"  required id="username" />
         </div>
-        <div className='control'>
+        <div className='control1'>
             <label htmlFor='password'>Password</label>
             <input type="password"  required id="password" />
         </div>
-        <div className='actions'>
+        <div className='actions1'>
             <button>Log in</button>
         </div>
     </form>
