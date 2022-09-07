17869e8a5ce74d1859095bac45b64f36fe57a0eb
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-09-07 16:57:41 +0300

    add log in form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import '../../NewRestarauntform.scss'
 
 
 function Login() {
   return (
-    <div>log in</div>
+    <div className='card'>
+    <form className='form' >
+    <div className='control'>
+            <label htmlFor='image'>Restaraunt Image</label>
+            <input type="url"  required id="image" />
+     </div>
+    <div className='control'>
+            <label htmlFor='text'>Restaraunt Name</label>
+            <input type="text"  required id="name" />
+        </div>
+        <div className='control'>
+            <label htmlFor='location'>Restaraunt Location</label>
+            <input type="text"  required id="location" />
+        </div>
+        <div className='control'>
+            <label htmlFor='number'>Phone Number</label>
+            <input type="text"  required id="number" />
+        </div>
+        <div className='control'>
+            <label htmlFor='description'>Restaraunt Description</label>
+            <textarea id="description" required rows='5' />
+        </div>
+        <div className='actions'>
+            <button>Add New Restaraunt</button>
+        </div>
+    </form>
+  </div>
   )
 }
 
